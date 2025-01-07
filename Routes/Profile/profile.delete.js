707c85e88faa618942639b545552d4991d4da6ca
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2025-01-07 13:39:50 +0100

    Also created the delete file functionality

--- Routes/Profile/profile.delete.js
+++ Routes/Profile/profile.delete.js
@@ -1,7 +1,20 @@
 const express = require("express");
 
+// Profile Model
+const { Profile } = require('../../Model/profile');
+
 const router = express.Router();
 
-router.delete("specific", (req, res) => {})
+router.delete("/delete/:id", async (req, res) => {
+    try{
+        const res = await Profile.findByIdAndDelete(req.params.id);
+
+        if(!res) res.status(400).send({ msg: `Unable to delete file`, data: null })
+
+        res.status(200).send({ msg: `Image Deleted!`, data: res });
+    } catch(err) {
+        res.status(400).send({ msg: "An error occured!", data: null });
+    }
+})
 
 module.exports = router;
