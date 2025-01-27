e3e98e1ac162f780e9cfd0159b8e497b096e902e
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2025-01-27 09:29:56 +0100

    Removed the limit to the image size

--- Middlewares/media.js
+++ Middlewares/media.js
@@ -4,9 +4,9 @@ const multer = require('multer');
 
 // Multer initailazation
 const upload = multer({ 
-    limits: {
-        fileSize: 2 * 1024 * 1024
-    },
+    // limits: {
+    //     fileSize: 2 * 1024 * 1024
+    // },
     fileFilter(req, file, cb) {
         if(!file.originalname.match(/\.(png|jpg|jpeg)$/)){
             return cb(new Error("Please enter an png, jpg or jpeg image"))
